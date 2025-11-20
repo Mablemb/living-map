70b3b9b199ebb9418245ffb23901a8e4e796f24e
Mablemb <mablemb@gmail.com>
2025-11-20 15:39:28 -0300

    feat: enhance entrypoint script with additional environment variable exports and debug information

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 set -e
 
+export DJANGO_SETTINGS_MODULE=setup.settings
+
 echo "==> Running migrations"
 python manage.py migrate --noinput
 
@@ -13,8 +15,11 @@ import os
 from django.conf import settings
 print("DEBUG=", settings.DEBUG)
 print("ALLOWED_HOSTS=", settings.ALLOWED_HOSTS)
+print("DEFAULT_FILE_STORAGE=", settings.DEFAULT_FILE_STORAGE)
+print("MEDIA_URL=", settings.MEDIA_URL)
 print("AWS_STORAGE_BUCKET_NAME=", os.getenv('AWS_STORAGE_BUCKET_NAME'))
 print("AWS_S3_CUSTOM_DOMAIN=", os.getenv('AWS_S3_CUSTOM_DOMAIN'))
+print("AWS_MEDIA_LOCATION=", os.getenv('AWS_MEDIA_LOCATION'))
 PY
 
 echo "==> Starting gunicorn"

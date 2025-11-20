bae0f8a6cd5fc394fd081bc7bd23014b4f49520c
Mablemb <mablemb@gmail.com>
2025-11-20 15:56:15 -0300

    fix: update debug output in entrypoint script to display STORAGES instead of DEFAULT_FILE_STORAGE

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,7 @@ django.setup()
 from django.conf import settings
 print("DEBUG=", settings.DEBUG)
 print("ALLOWED_HOSTS=", settings.ALLOWED_HOSTS)
-print("DEFAULT_FILE_STORAGE=", settings.DEFAULT_FILE_STORAGE)
+print("STORAGES=", settings.STORAGES)
 print("MEDIA_URL=", settings.MEDIA_URL)
 print("AWS_STORAGE_BUCKET_NAME=", os.getenv('AWS_STORAGE_BUCKET_NAME'))
 print("AWS_S3_CUSTOM_DOMAIN=", os.getenv('AWS_S3_CUSTOM_DOMAIN'))
